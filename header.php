d25a6cadb5e71b783497929b90b5384c91865b98
Austin Crane <austincrane@gmail.com>
2018-05-04 12:35:06 -0400

    change fonts

--- header.php
+++ header.php
@@ -28,6 +28,7 @@
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 <?php wp_head(); ?>
 
 
